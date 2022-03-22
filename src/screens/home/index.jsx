456b7a468f375cd42aa0b66c34b0f6e7328685a8
codeuiandy <frostandy41@gmail.com>
2022-03-22 12:36:51 +0100

    updates

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -496,15 +496,13 @@ export default function Index() {
                 aria-controls="panel1d-content"
                 id="panel1d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography>How do I create a QC Express Account?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  Simply log on to the QC Express platform and sign up. An
+                  account is created for you instantly upon the completion of
+                  the short form on our platform.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -516,15 +514,12 @@ export default function Index() {
                 aria-controls="panel2d-content"
                 id="panel2d-header"
               >
-                <Typography>Collapsible Group Item #2</Typography>
+                <Typography>How do I book a shipment pick-up?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  Easy. Just log on the QC Express platform and click on
+                  “request a pick-up”
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -536,19 +531,16 @@ export default function Index() {
                 aria-controls="panel3d-content"
                 id="panel3d-header"
               >
-                <Typography>Collapsible Group Item #3</Typography>
+                <Typography>What destinations can I ship to?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  We deliver to all locations within Nigeria and over 220
+                  countries globally. Please consult our coverage areas on our
+                  website.
                 </Typography>
               </AccordionDetails>
             </Accordion>
-
             <Accordion
               expanded={expanded === "panel4"}
               onChange={handleChange("panel4")}
@@ -557,19 +549,15 @@ export default function Index() {
                 aria-controls="panel4d-content"
                 id="panel4d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography>Can I send anything on QC Express?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  Please consult our Restricted and Prohibited items list on the
+                  QC Express platform/website.
                 </Typography>
               </AccordionDetails>
             </Accordion>
-
             <Accordion
               expanded={expanded === "panel5"}
               onChange={handleChange("panel5")}
@@ -578,19 +566,16 @@ export default function Index() {
                 aria-controls="panel5d-content"
                 id="panel5d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography>How do I calculate my charges?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  A delivery charge is automatically calculated for you once you
+                  specify the weight, pick-up and delivery locations on our
+                  platform.
                 </Typography>
               </AccordionDetails>
             </Accordion>
-
             <Accordion
               expanded={expanded === "panel6"}
               onChange={handleChange("panel6")}
@@ -599,19 +584,17 @@ export default function Index() {
                 aria-controls="panel6d-content"
                 id="panel6d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography>
+                  Can I estimate the size and weight of a parcel?
+                </Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  No. Actual weights of shipments must be determined before
+                  shipping
                 </Typography>
               </AccordionDetails>
             </Accordion>
-
             <Accordion
               expanded={expanded === "panel7"}
               onChange={handleChange("panel7")}
@@ -620,15 +603,298 @@ export default function Index() {
                 aria-controls="panel7d-content"
                 id="panel7d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography>
+                  Can I use my own packaging when shipping?
+                </Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  Yes. You are primarily responsible for packaging
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              expanded={expanded === "panel8"}
+              onChange={handleChange("panel8")}
+            >
+              <AccordionSummary
+                aria-controls="panel8d-content"
+                id="panel8d-header"
+              >
+                <Typography>
+                  I need to cancel a shipment, what should I do?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Please contact Customer Service at: support@quartzclassic.com
+                   
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel9"}
+              onChange={handleChange("panel9")}
+            >
+              <AccordionSummary
+                aria-controls="panel9d-content"
+                id="panel9d-header"
+              >
+                <Typography>
+                  Can I change or cancel a pickup I scheduled online?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  No. Please contact Customer Service at:
+                  support@quartzclassic.com
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel10"}
+              onChange={handleChange("panel10")}
+            >
+              <AccordionSummary
+                aria-controls="panel10d-content"
+                id="panel10d-header"
+              >
+                <Typography>
+                  What should I do if the courier can’t take a parcel and I was
+                  already charged for the shipment?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  This doesn't happen often, but if a courier arrived but could
+                  not take the parcel, it could be because it contains a
+                  prohibited item or didn’t have the necessary paperwork ready.
+                  We’ll help you arrange a new pickup or refund. Please contact
+                  Customer Service at: support@quartzclassic.com
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel11"}
+              onChange={handleChange("panel11")}
+            >
+              <AccordionSummary
+                aria-controls="panel11d-content"
+                id="panel11d-header"
+              >
+                <Typography>
+                  The courier could not take my shipment because it is too
+                  big/heavy for their vehicle. What should I do?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  If your shipment is very large in size or heavy in weight: It
+                  may require special pickup equipment and a different vehicle.
+                  Please contact us to reschedule your pickup.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel12"}
+              onChange={handleChange("panel12")}
+            >
+              <AccordionSummary
+                aria-controls="panel12d-content"
+                id="panel12d-header"
+              >
+                <Typography>
+                  I have tracked a shipment and been told to contact Customer
+                  Service. Why is this?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  This usually occurs if the information we have is insufficient
+                  to track the shipment for you. The address may be incorrect,
+                  and we require clarification or some additional information
+                  may be required. The matter can be resolved very quickly
+                  by contacting Customer Service.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel13"}
+              onChange={handleChange("panel13")}
+            >
+              <AccordionSummary
+                aria-controls="panel13d-content"
+                id="panel13d-header"
+              >
+                <Typography>
+                  My shipment is showing “Custom status updated” from the
+                  destination, even though the shipment just left the country.
+                  What does this mean?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  This status provides an indication of the clearance processing
+                  from the destination. Please click in to the further details
+                  (+) to open and see additional information available. This
+                  checkpoint allows you to see very early if there is additional
+                  information required by customs for clearance.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel14"}
+              onChange={handleChange("panel14")}
+            >
+              <AccordionSummary
+                aria-controls="panel14d-content"
+                id="panel14d-header"
+              >
+                <Typography>
+                  If I do not have my tracking number, is it still possible to
+                  track my shipment?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Yes, under certain conditions. We strongly advise you to make
+                  a record of your tracking number however, if you have the
+                  shipper's reference number, you can use our online Shipper’s
+                  Reference Tracking service.  
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel15"}
+              onChange={handleChange("panel15")}
+            >
+              <AccordionSummary
+                aria-controls="panel15d-content"
+                id="panel15d-header"
+              >
+                <Typography>How are customs duties calculated?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  The calculation of duties depends on the assessable value of a
+                  dutiable shipment. For the purpose of this calculation,
+                  dutiable goods are given a classification code that is known
+                  as the Harmonized System code. Whether a shipment is a gift or
+                  not, it must still go through an import procedure as
+                  determined by custom’s law in the destination country. The
+                  shipment is cleared through customs based on the origin
+                  country, the value and quantity, but not its purpose. Dutiable
+                  shipments are subject to a customs duty, which is a tariff or
+                  tax imposed on goods when transported across international
+                  borders. This system has been assigned by the World Customs
+                  Organization and continues to evolve.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel16"}
+              onChange={handleChange("panel16")}
+            >
+              <AccordionSummary
+                aria-controls="panel16d-content"
+                id="panel16d-header"
+              >
+                <Typography>
+                  Who is responsible for payment of duties and taxes?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  The payment of duties and taxes are typically the
+                  responsibility of the receiver.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel17"}
+              onChange={handleChange("panel17")}
+            >
+              <AccordionSummary
+                aria-controls="panel17d-content"
+                id="panel17d-header"
+              >
+                <Typography>
+                  Why is the receiver being asked to pay Customs duties and
+                  taxes?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Duties and taxes are levied by Customs in the destination
+                  country and the receiver is responsible for paying them. The
+                  Carrier will collect payment from the receiver prior to
+                  delivery. Generally, we are unable to deliver parcels until
+                  these have been paid.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel18"}
+              onChange={handleChange("panel18")}
+            >
+              <AccordionSummary
+                aria-controls="panel18d-content"
+                id="panel18d-header"
+              >
+                <Typography>
+                  I’m sending a gift – can I pay duties and taxes instead of the
+                  receiver?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  You need an account to pay duties and taxes for the receiver.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>{" "}
+            <Accordion
+              expanded={expanded === "panel19"}
+              onChange={handleChange("panel19")}
+            >
+              <AccordionSummary
+                aria-controls="panel19d-content"
+                id="panel19d-header"
+              >
+                <Typography>
+                  Why doesn’t QC include duties and taxes in its prices?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  The customs authorities in the destination country determine
+                  whether any duties and taxes are applicable when the parcel
+                  arrives. This is based on the information provided on the
+                  shipping label (or waybill) and customs paperwork – in
+                  particular the shipment contents, declared value and weight.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              expanded={expanded === "panel20"}
+              onChange={handleChange("panel20")}
+            >
+              <AccordionSummary
+                aria-controls="panel20d-content"
+                id="panel20d-header"
+              >
+                <Typography>
+                  Why do I need to pay customs duty if I received a shipment as
+                  a gift?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Whether a shipment is a gift or not, it must still go through
+                  an import procedure as determined by custom’s law in the
+                  destination country. The shipment is cleared through customs
+                  based on the origin country, the value and quantity, but not
+                  its purpose. You will need to pay customs duty and taxes only
+                  if the value of the gift is above a stated local threshold.
                 </Typography>
               </AccordionDetails>
             </Accordion>
