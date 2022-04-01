35f9d9b724203857d8797a86804ffbdde904288a
gospel4me <tracy@gloqal.co>
2022-04-01 16:49:11 +0100

    m

--- src/components/layout/inAppNavbar.jsx
+++ src/components/layout/inAppNavbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Logo } from "../../assets/svg";
 import { useHistory } from "react-router-dom";
-import "./nav.css"
+import "./nav.css";
 import ps from "../../assets/ps.svg";
 import as from "../../assets/as.svg";
 export default function InAppNavbar() {
@@ -14,25 +14,39 @@ export default function InAppNavbar() {
             <Logo />
           </li>
           <li>Cockpit</li>
-          <li className="dropdown">Shipments
-          <div class="dropdown-content" style={{width:"200px"}}>
-          <li style={{marginLeft:"0px",marginTop:"10px"}}>Book Shipment</li>
-          <li style={{marginLeft:"0px",marginTop:"10px"}}>Drop-off Location</li>
-          <li style={{marginLeft:"0px",marginTop:"10px"}}>Track Shipment</li>
-          <li style={{marginLeft:"0px",marginTop:"10px"}}>Shipping History</li>
-          <li style={{marginLeft:"0px",marginTop:"10px"}}>Tell a friend</li>
-
-           </div>
+          <li className="dropdown">
+            Shipments
+            <div class="dropdown-content" style={{ width: "200px" }}>
+              <li style={{ marginLeft: "0px", marginTop: "10px" }}>
+                Book Shipment
+              </li>
+              <li style={{ marginLeft: "0px", marginTop: "10px" }}>
+                Drop-off Location
+              </li>
+              <li style={{ marginLeft: "0px", marginTop: "10px" }}>
+                Track Shipment
+              </li>
+              <li style={{ marginLeft: "0px", marginTop: "10px" }}>
+                Shipping History
+              </li>
+              <li style={{ marginLeft: "0px", marginTop: "10px" }}>
+                Tell a friend
+              </li>
+            </div>
           </li>
           <li>Account</li>
-
         </ul>
-      
       </div>
 
-      <div className="inAppNavbarCol2" style={{display:"flex"}}>
-        <button style={{marginLeft:"10px"}}>Download IOS</button>
-        <button  style={{marginLeft:"10px"}}>Download Playstore</button>
+      <div className="inAppNavbarCol2">
+          <div className="row" style={{width:"30%",float:"right"}}>
+            <div className="col">
+              <img src={ps} alt="" style={{width:"100%"}}/>
+            </div>
+            <div className="col"style={{marginLeft:"-30px"}} >
+              <img src={as} alt=""  style={{width:"100%"}}/>
+            </div>
+          </div>
 
       </div>
     </div>
